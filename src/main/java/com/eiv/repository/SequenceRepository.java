7b22a6ffe51af67467f16304fe150189c0258b02
Diego Cairone <diegocairone@gmail.com>
2020-04-28 12:46:44 -0300

    Limpieza Das y Repository para Sequence

--- src/main/java/com/eiv/repository/SequenceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eiv.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.eiv.entity.SequenceEntity;
-
-public interface SequenceRepository extends JpaRepository<SequenceEntity, String> {
-
-}
